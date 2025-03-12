74c8db7333263900ba88a2bff7b92124ea13f23c
Shantanu18 <shantanusk2002@gmail.com>
2025-03-13 01:01:40 +0530

    Push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import LandingPage from "./Components/LandingPage";
+import Todo from "./Components/Todo";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </Router>
     
